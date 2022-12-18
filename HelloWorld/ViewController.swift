6bdb1f3fff3bca79c1f9aa498ad1488eb5849313
doomout <winquake3@hanmail.net>
2022-12-18 13:18:13 +0900

    텍스트 입력 버튼 기능 구현

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var lblHello: UILabel! //출력 레이블용 아웃렛 변수
+    
+    @IBOutlet var txtName: UITextField! //이름 입력용 아웃렛 변수
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func btnSend(_ sender: UIButton) {
+        lblHello.text = "Hello, " + txtName.text!
+    }
+    
 }
 
